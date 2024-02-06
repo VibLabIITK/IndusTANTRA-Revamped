969be517f1eb95e20f91703b0aaa848c05e62fef
YuvalBansal <yuvalbansal200.yb@gmail.com>
2024-02-06 18:15:09 +0530

    Added Hyperlinks

--- src/pages/projects/news.js
+++ src/pages/projects/news.js
@@ -5,15 +5,15 @@ export const News = () => {
         <div className="page">
             <h1 className="header">News</h1>
             <h3>Industry 4.0</h3>
-            <p><a href="https://pib.gov.in/PressReleasePage.aspx?PRID=1586545">PIB</a><br />
-                <a href="https://cio.economictimes.indiatimes.com/news/strategy-and-management/industry-4-0-railways-to-integrate-big-data-ai/71369653">ETCIO</a><br />
-                <a href="https://swarajyamag.com/news-brief/industry-40-powered-by-indian-railways-iit-kanpur-heres-what-indias-first-smart-coach-factory-will-look-like">Swarajya</a><br />
-                <a href="https://www.deccanherald.com/national/north-and-central/railways-launches-pilot-project-to-usher-industry-40-764727.html">Deccan Herald</a><br />
+            <p><a href="https://pib.gov.in/PressReleasePage.aspx?PRID=1586545" target='__blank'>PIB</a><br />
+                <a href="https://cio.economictimes.indiatimes.com/news/strategy-and-management/industry-4-0-railways-to-integrate-big-data-ai/71369653" target='__blank'>ETCIO</a><br />
+                <a href="https://swarajyamag.com/news-brief/industry-40-powered-by-indian-railways-iit-kanpur-heres-what-indias-first-smart-coach-factory-will-look-like" target='__blank'>Swarajya</a><br />
+                <a href="https://www.deccanherald.com/national/north-and-central/railways-launches-pilot-project-to-usher-industry-40-764727.html" target='__blank'>Deccan Herald</a><br />
                 ANI<br />
-                <a href="https://www.business-standard.com/article/news-ani/pilot-project-at-rae-bareli-coach-factory-to-usher-industry-4-0-119092800860_1.html">Business Standard</a><br />
-                <a href="http://www.uniindia.com/~/railways-dept-of-s-t-bats-for-project-on-industry-4-0-at-mcf-raebareli/India/news/1743301.html">UNI</a><br />
-                <a href="https://www.outlookindia.com/newsscroll/pilot-project-to-introduce-industry-40-launched-by-railways/1629469">Outlook</a><br />
-                <a href="https://www.ndtv.com/education/industry-4-0-railway-ministry-and-iit-kanpur-digital-project-to-host-ai-big-data-machine-learning-2108763">NDTV Financial Express</a>
+                <a href="https://www.business-standard.com/article/news-ani/pilot-project-at-rae-bareli-coach-factory-to-usher-industry-4-0-119092800860_1.html" target='__blank'>Business Standard</a><br />
+                <a href="http://www.uniindia.com/~/railways-dept-of-s-t-bats-for-project-on-industry-4-0-at-mcf-raebareli/India/news/1743301.html" target='__blank'>UNI</a><br />
+                <a href="https://www.outlookindia.com/newsscroll/pilot-project-to-introduce-industry-40-launched-by-railways/1629469" target='__blank'>Outlook</a><br />
+                <a href="https://www.ndtv.com/education/industry-4-0-railway-ministry-and-iit-kanpur-digital-project-to-host-ai-big-data-machine-learning-2108763" target='__blank'>NDTV Financial Express</a>
             </p>
             <h3>Technology Mission for Indian Railways (TMIR)</h3>
             <p><a href={file} target='__blank'>Rail Budget (2015) Announcement</a></p>
--- src/pages/projects/projects.js
+++ src/pages/projects/projects.js
@@ -1,30 +1,45 @@
+import pilotproject from '../../utilities/files/TMIR_Pilot_Project.pdf';
+import brief from '../../utilities/files/TMIR_Brief.pdf';
+import micc_constitution from '../../utilities/files/MICCConstitution.pdf';
+import chairman from '../../utilities/files/Chairman.pdf';
+import mission from '../../utilities/files/Mission.pdf';
+import extension from '../../utilities/files/Extension.pdf';
+
 export const Projects = () => {
     return (
         <div className="page">
             <h1>Major Projects</h1>
             <div className="projects">
                 <h3>Technology Mission for Indian Railways (TMIR)</h3>
-                <p>Pilot Project on Implementation of Industry 4.0 Protocols at Modern Coach Factory (MCF), Raebareli<br />
-                    Rail Budget (2015) Announcement<br />
-                    Mission Implementation & Coordination Committee (MICC): Notification<br />
-                    Chairman, MICC-TMIR: Notification<br />
-                    ToI<br />
-                    Mission Vision<br />
-                    Mission Extension<br />
+                <p><a href={pilotproject} target='__blank'>Pilot Project on Implementation of Industry 4.0 Protocols at Modern Coach Factory (MCF), Raebareli</a><br />
+                    <a href={brief} target='__blank'>Rail Budget (2015) Announcement</a><br />
+                    <a href={micc_constitution} target='__blank'>Mission Implementation & Coordination Committee (MICC): Notification</a><br />
+                    <a href={chairman} target='__blank'>Chairman, MICC-TMIR: Notification</a><br />
+                    <a href="https://timesofindia.indiatimes.com/city/kanpur/Rlys-appoint-IIT-K-Prof-as-TMIR-chief/articleshow/47185232.cms" target='__blank'>ToI</a><br />
+                    <a href={mission}>Mission Vision</a><br />
+                    <a href={extension}>Mission Extension</a><br />
                 </p>
                 <h3>Technology Mission on Railway Safety (TMRS)</h3>
-                <p>Technology Mission on Railway Safety (TMRS) - Indian Railways</p>
+                <p>
+                    <a href="https://home.iitk.ac.in/~vyas/tmrs/index.html">Technology Mission on Railway Safety (TMRS) - Indian Railways</a>
+                </p>
                 <h3>Wheel Impact Load Detection System (WILD)</h3>
-                <p>Design and Development of Instrumentation System for Wheel-Flat Detection in Indian Railways RDSO, 2001<br />
-                    Wheel Impact Load Detection System (WILD) - RDSO</p>
+                <p>
+                    Design and Development of Instrumentation System for Wheel-Flat Detection in Indian Railways RDSO, 2001<br />
+                    <a href="https://home.iitk.ac.in/~vyas/WILD/systemdescription.html">Wheel Impact Load Detection System (WILD) - RDSO</a>
+                </p>
                 <h3>Electronic Stability Program for Automobiles</h3>
-                <p>Consortium project with IIT Bombay, TATA Motors, Mahindra & Mahindra, Ashok Leyland, TCS, Pricol and others on development of
+                <p>
+                    Consortium project with IIT Bombay, TATA Motors, Mahindra & Mahindra, Ashok Leyland, TCS, Pricol and others on development of
                     ABS (Anti-lock Brake Systems), TCS (Traction Control Systems) and ESC (Electronic Stabilty Control) for automobiles – supported
                     by the CAR ( Core group on Automotive Research) – TIFAC, DST.<br />
-                    Electronic Stability Program for Automobiles - CAR</p>
+                    <a href="https://home.iitk.ac.in/~vyas/ABS/ABS.html">Electronic Stability Program for Automobiles - CAR</a>
+                </p>
                 <h3>IITK NanoSat</h3>
-                <p>Design, Development and Launch of a micro-satellite based on MEMS technology. Supported by ISRO.<br />
-                    JUGNU (NanoSatellite IIT Kanpur)</p>
+                <p>
+                    Design, Development and Launch of a micro-satellite based on MEMS technology. Supported by ISRO.<br />
+                    <a href="https://home.iitk.ac.in/~vyas/Jugnu/index.html">JUGNU (NanoSatellite IIT Kanpur)</a>
+                </p>
                 <h3>B-SMART</h3>
                 <p>(Board for Smart Materials Research and Technology)</p>
                 <h3>NPMASS</h3>
@@ -47,6 +62,24 @@ export const Projects = () => {
                 <p>Hindustan Aeronautics Ltd., 2002</p>
                 <h3>Output Only Modal Analysis for Rotor-Bearing Systems</h3>
                 <p>Aeronautical Research And Development Board, Ministry of Defence, Govt. of India, 2002 ---</p>
+                <h1>About 4i Lab</h1>
+                <p>4i-Lab is a recent initiative at the Indian institute of Technology, Kanpur. It is an enabling
+                    laboratory with an objective to facilitate design evolution into complete products. The four "i"s
+                    stand for Innovation, Integration, Incubation and Implementation. The laboratory is envisaged as a
+                    central facility for concept design and product realization. The laboratory environment is structured
+                    to digitally integrate the processes of design, simulation and manufacturing. The facility has been
+                    designed to take care of professional course requirements of students and to provide infrastructural
+                    facilities for sponsored research and industrial consultancy.</p>
+                <h3>Facilities</h3>
+                <p>
+                    <ul>
+                        <li>CNC Vertical Milling Center</li>
+                        <li>CNC Turning Center</li>
+                        <li>Abrasive Water Jet Machining Center</li>
+                        <li>Laser Engraving & Cutting Machine</li>
+                        <li>Rapid Prototyping Machine</li>
+                    </ul>
+                </p>
             </div>
         </div >
     );
